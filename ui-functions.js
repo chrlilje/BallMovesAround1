c4d715a75019d957b064eebbd5a2c0c3d44b134f
Christian Liljedahl <christian@liljedahl.dk>
2025-02-25 21:38:06 +0100

    Added and removed gravity

--- ui-functions.js
+++ ui-functions.js
@@ -4,7 +4,9 @@ function mousePressed() {
     let style = {
       color: color(random(255), random(255), random(255))
     };
-    let b = new Ball(width / 2, height / 2, 20);
+//    let b = new Ball(width / 2, height / 2, 20);
+    let b = new Ball(width / 2, height / 2, 20, {});
+
     b.style = style;
     // Set the velocity of the ball to 3 and the direction to the direction of the mouse
     b.setVelocity(3, Math.atan2(mouseY - b.y, mouseX - b.x));
